8ceb082cd515eaac6f213b033c043222d726e5d2
Angelo31294 <luongoangelo1@gmail.com>
2022-10-26 12:44:08 +0200

    Fine

---
+++ jsnak5/script.js
@@ -0,0 +1,15 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+let arrey = [];
+
+for (let i = 0; i < 6; i++) {
+    let num = Number(prompt("Inserisci un numero"));
+    
+    if ( num % 2 != 0 ){
+        arrey.push(num);
+    }
+    
+}
+
+console.log(arrey);
